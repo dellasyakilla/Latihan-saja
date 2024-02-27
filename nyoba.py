bdc21b0ca62427f6f0c878e095266f5368d130c9
dellasyakilla <syakilladella7@gmail.com>
2024-02-27 18:15:38 +0700

    Commit Latihan Pertamaku

---
+++ nyoba.py
@@ -0,0 +1,8 @@
+print ("Hallo Guys..")
+
+nama = "Della Syakilla"
+pekerjaan = "Mahasiswa"
+kota = "Bandung"
+
+print ("Aku" + nama + ", seorang " + pekerjaan + ",dan aku tinggal di " + kota + "tercinta")
+print ("Terimakasih")
